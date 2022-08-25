e26d666b74da4680715dd13a8f048498e6e81015
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-08-25 11:21:39 +0200

    finished main work

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ let getComputerChoice = () => {
     
     switch(choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
@@ -25,11 +25,59 @@ let getPlayerChoice = (playerSelection) => {
     }
 }
 
-/*let playGame = (playerSelection, computerSelection) => {
+let playGame = (playerSelection, computerSelection) => {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scisors beats Paper"
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors"
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You win! Paper beats Rock"
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You win! Scisors beats Paper"
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! Rock beats Scissors"
+    }
+    else {
+        return "Thats a tie!"
+    }
+}
 
-}*/
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        result = playGame(getPlayerChoice("rock"), getComputerChoice());
+        console.log(result)
+        if (result.includes("win")) {
+            playerScore += 1;
+        }
+        else if (result.includes("lose")) {
+            computerScore += 1;
+        }
+        else if (result.includes("tie")) {
+            //do nothing
+        }
+    }
+
+    //console.log(playerScore)
+    //console.log(computerScore)
+
+    let end = (playerScore > computerScore) ? `You won with ${playerScore} to ${computerScore}!` :
+        (playerScore < computerScore) ? `You lost with ${playerScore} to ${computerScore}!` :
+        `You tied with ${playerScore} to ${computerScore}!`;
+    
+    console.log(end)
+}
 
 const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice("rock");
 
-//console.log(getPlayerChoice(playerSelection))
+game();
