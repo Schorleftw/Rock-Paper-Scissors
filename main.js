9ef61ce57022417b928ecee2ff1eeb62b6561404
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-10-24 13:55:25 +0200

    logic finished

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Make Buttons accessible
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+let playerScore = 0;
+let computerScore = 0;
+
 // Relevant for computer choice
 let random = () => {
     return Math.floor(Math.random() * 3);
@@ -49,36 +56,83 @@ let compare = (playerSelection, computerSelection) => {
     else if (computerSelection === "scissors" && playerSelection === "rock") {
         return "You win! Rock beats Scissors"
     }
-    else {
+    else if (playerSelection === computerSelection) {
         return "Thats a tie!"
     }
 }
 
 // Main function for playing the game
 
-let game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        result = compare(getPlayerChoice(prompt()), getComputerChoice());
-        console.log(result)
-        if (result.includes("win")) {
-            playerScore += 1;
-        }
-        else if (result.includes("lose")) {
-            computerScore += 1;
-        }
-        else if (result.includes("tie")) {
-            //do nothing
-        }
+window.addEventListener("click", function getPlayerChoice(e) {
+    let choice;
+    let result;
+
+
+    if (e.target.innerHTML === "Rock" || e.target.innerHTML === "Paper" || e.target.innerHTML === "Scissors") {
+        choice = e.target.innerHTML;
+        choice = choice.toLowerCase();
+        result = compare(choice, getComputerChoice());
+    }
+    else {
+        //do nothing
     }
 
-    let end = (playerScore > computerScore) ? `You won with ${playerScore} to ${computerScore}!` :
-        (playerScore < computerScore) ? `You lost with ${playerScore} to ${computerScore}!` :
-        `You tied with ${playerScore} to ${computerScore}!`;
+    if (result !== undefined && result.includes("win")) {
+        playerScore += 1;
+    }
+    else if (result !== undefined && result.includes("lose")) {
+        computerScore += 1;
+    }
+    else if (result !== undefined && result.includes("tie")) {
+        //do nothing
+    }
+    else if (playerScore === 5 || computerScore === 5) {
+        console.log(end)
+    }
+
+    console.log("Player score" + playerScore);
+    console.log("Computer score" + computerScore);
+
+    if (playerScore === 5) {
+        console.log("You did it! You won")
+    }
+    else if (computerScore === 5) {
+        console.log("The computer defeated you!")
+    }
+});
+
+
+
+// rock.addEventListener("click", function game() {
+//     //get inner HTML and place it in variable
+//     //get computer choice
+//     //compare computer and player choice and set score
+//     //after 5 points declare winner
+// });
+
+// let game = () => {
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     // for (let i = 0; i < 5; i++) {
+//     //     result = compare(getPlayerChoice(prompt()), getComputerChoice());
+//     //     console.log(result)
+//     //     if (result.includes("win")) {
+//     //         playerScore += 1;
+//     //     }
+//     //     else if (result.includes("lose")) {
+//     //         computerScore += 1;
+//     //     }
+//     //     else if (result.includes("tie")) {
+//     //         //do nothing
+//     //     }
+//     // }
+
+//     // let end = (playerScore > computerScore) ? `You won with ${playerScore} to ${computerScore}!` :
+//     //     (playerScore < computerScore) ? `You lost with ${playerScore} to ${computerScore}!` :
+//     //     `You tied with ${playerScore} to ${computerScore}!`;
     
-    console.log(end)
-}
+//     // console.log(end)
+// }
 
 //game();
